84de7a0b172f90966de30bab3b0f59fa314352ca
FFFFF <woshitiancai1014@gmail.com>
2025-05-05 09:45:50 +0800

    feat: stories component modified

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -3,138 +3,25 @@ import React from "react";
 
 export default function Stories() {
   return (
-    <div className="p-4 bg-white rotate-lg shadow-md text-sm rounded-lg overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+    <div className="p-4 bg-white rotate-lg shadow-md text-sm rounded-lg overflow-auto horizontal-scroll">
       <div className="flex gap-8 w-max">
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://cdn.dribbble.com/userupload/9082089/file/original-28361a132344f19913b167ae33e93d12.png?resize=752x&vertical=center"
-            width={100}
-            height={100}
-            alt="avatar"
-            className="w-15 h-15 rounded-full"
-          />
-          <span className="font-medium">FFFF</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://cdn.dribbble.com/userupload/9082089/file/original-28361a132344f19913b167ae33e93d12.png?resize=752x&vertical=center"
-            width={100}
-            height={100}
-            alt="avatar"
-            className="w-15 h-15 rounded-full"
-          />
-          <span className="font-medium">FFFF</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://cdn.dribbble.com/userupload/9082089/file/original-28361a132344f19913b167ae33e93d12.png?resize=752x&vertical=center"
-            width={100}
-            height={100}
-            alt="avatar"
-            className="w-15 h-15 rounded-full"
-          />
-          <span className="font-medium">FFFF</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://cdn.dribbble.com/userupload/9082089/file/original-28361a132344f19913b167ae33e93d12.png?resize=752x&vertical=center"
-            width={100}
-            height={100}
-            alt="avatar"
-            className="w-15 h-15 rounded-full"
-          />
-          <span className="font-medium">FFFF</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://cdn.dribbble.com/userupload/9082089/file/original-28361a132344f19913b167ae33e93d12.png?resize=752x&vertical=center"
-            width={100}
-            height={100}
-            alt="avatar"
-            className="w-15 h-15 rounded-full"
-          />
-          <span className="font-medium">FFFF</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://cdn.dribbble.com/userupload/9082089/file/original-28361a132344f19913b167ae33e93d12.png?resize=752x&vertical=center"
-            width={100}
-            height={100}
-            alt="avatar"
-            className="w-15 h-15 rounded-full"
-          />
-          <span className="font-medium">FFFF</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://cdn.dribbble.com/userupload/9082089/file/original-28361a132344f19913b167ae33e93d12.png?resize=752x&vertical=center"
-            width={100}
-            height={100}
-            alt="avatar"
-            className="w-15 h-15 rounded-full"
-          />
-          <span className="font-medium">FFFF</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://cdn.dribbble.com/userupload/9082089/file/original-28361a132344f19913b167ae33e93d12.png?resize=752x&vertical=center"
-            width={100}
-            height={100}
-            alt="avatar"
-            className="w-15 h-15 rounded-full"
-          />
-          <span className="font-medium">FFFF</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://cdn.dribbble.com/userupload/9082089/file/original-28361a132344f19913b167ae33e93d12.png?resize=752x&vertical=center"
-            width={100}
-            height={100}
-            alt="avatar"
-            className="w-15 h-15 rounded-full"
-          />
-          <span className="font-medium">FFFF</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://cdn.dribbble.com/userupload/9082089/file/original-28361a132344f19913b167ae33e93d12.png?resize=752x&vertical=center"
-            width={100}
-            height={100}
-            alt="avatar"
-            className="w-15 h-15 rounded-full"
-          />
-          <span className="font-medium">FFFF</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://cdn.dribbble.com/userupload/9082089/file/original-28361a132344f19913b167ae33e93d12.png?resize=752x&vertical=center"
-            width={100}
-            height={100}
-            alt="avatar"
-            className="w-15 h-15 rounded-full"
-          />
-          <span className="font-medium">FFFF</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://cdn.dribbble.com/userupload/9082089/file/original-28361a132344f19913b167ae33e93d12.png?resize=752x&vertical=center"
-            width={100}
-            height={100}
-            alt="avatar"
-            className="w-15 h-15 rounded-full"
-          />
-          <span className="font-medium">FFFF</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://cdn.dribbble.com/userupload/9082089/file/original-28361a132344f19913b167ae33e93d12.png?resize=752x&vertical=center"
-            width={100}
-            height={100}
-            alt="avatar"
-            className="w-15 h-15 rounded-full"
-          />
-          <span className="font-medium">FFFF</span>
-        </div>
+        {Array(10)
+          .fill(0)
+          .map((_, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center gap-2 cursor-pointer"
+            >
+              <Image
+                src="https://cdn.dribbble.com/userupload/9082089/file/original-28361a132344f19913b167ae33e93d12.png?resize=752x&vertical=center"
+                width={100}
+                height={100}
+                alt="story"
+                className="w-15 h-15 rounded-full"
+              />
+              <span className="font-medium">FFFF</span>
+            </div>
+          ))}
       </div>
     </div>
   );
