3e57985b9a1e028884de4df324cd656fb977c24a
FFFFF <woshitiancai1014@gmail.com>
2025-05-05 16:08:50 +0800

    feat: add some components

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
 
-export default function AddPost() {
+import { Album, Calendar, Video, Vote } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+export default async function AddPost() {
+
+
+  };
   return (
-    <div>
-      AddPost
+    <div className="p-4 bg-white rounded-lg flex gap-4 text-sm">
+      <div>
+        <Image
+          src="https://cdn.dribbble.com/userupload/22681624/file/original-8fa4fb07e0045edbdb3d6c5cd1814a18.gif"
+          width={60}
+          height={60}
+          alt="avatar"
+          className="rounded-full"
+        />
+      </div>
+      <div className="flex flex-col flex-1">
+        <form action={testAction} className="flex gap-4 items-end">
+          <textarea
+            placeholder="What's on your mind?"
+            className="w-full bg-slate-100 rounded-lg p-3"
+            name="desc"
+          ></textarea>
+          <Image
+            src="/emoji.png"
+            alt=""
+            width={20}
+            height={20}
+            className="w-5 h-5 cursor-pointer self-end"
+          />
+          <button>Send</button>
+        </form>
+
+        <div className="flex mt-3 gap-4">
+          <div className="flex items-center gap-1">
+            <Album className="h-5 w-5 text-emerald-500" />
+            <span>Photo</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <Video className="h-5 w-5 text-pink-500" />
+            <span>Video</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <Vote className="h-5 w-5 text-cyan-500" />
+            <span>Poll</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <Calendar className="h-5 w-5 text-orange-500" />
+            <span>Event</span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/RightMenu.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function RightMenu() {
-  return (
-    <div>
-      RightMenu
-    </div>
-  )
-}
